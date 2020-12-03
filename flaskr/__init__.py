66c5632d82b38da1026c43f5004771a29e568c0f
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-12-03 22:47:13 +0100

    Added support for heraku postgresql db

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from configparser import ConfigParser
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app(test_config=None):
@@ -7,6 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     from .api import api
+    from dbModel import db
     app.register_blueprint(api.bp)
 
     config = ConfigParser()
@@ -15,4 +17,7 @@ def create_app(test_config=None):
     app.config['SECRET_KEY'] = config['SECRET']['secret_key']
     app.config['IPSTACK_FIELDS'] = eval(config['DATA']['fields'])
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = config['DATABASE']['uri']
+
+    db.init_app(app)
     return app
