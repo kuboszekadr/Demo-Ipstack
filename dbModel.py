dcb96f50236ef7483112b1f6bc804ff71cb4da0f
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-12-03 22:46:39 +0100

    Added user relation and unified WebPage column naming

--- dbModel.py
+++ dbModel.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects import postgresql
+from flask import current_app
 
 db = SQLAlchemy()
 
@@ -7,8 +8,12 @@ db = SQLAlchemy()
 class WebPage(db.Model):
     __tablename__ = "d_web_page"
 
-    web_page_id = db.Column(db.Integer(), primary_key=True)
-    web_page_name = db.Column(db.String(255))
+    web_id = db.Column(db.Integer(),
+                       db.Sequence('d_web_page_web_id_seq'),
+                       primary_key=True
+                       )
+    web_ip = db.Column(db.String(255), primary_key=True)
+
     web_continent_name = db.Column(db.String(255))
     web_country_code = db.Column(db.String(255))
     web_country_name = db.Column(db.String(255))
@@ -21,3 +26,11 @@ class WebPage(db.Model):
 
     web_latitude = db.Column(db.Numeric())
     web_longitude = db.Column(db.Numeric())
+
+
+class User(db.Model):
+    __tablename__ = "d_user"
+
+    user_id = db.Column(db.Integer(), primary_key=True)
+    login = db.Column(db.String(50))
+    password = db.Column(db.String(100))
