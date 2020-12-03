b51fbf91c4c6945df0350267211e28c7cecd091a
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-12-03 22:48:26 +0100

    Post request support added

--- flaskr/api/api.py
+++ flaskr/api/api.py
@@ -1,5 +1,6 @@
 from .models import Ipstack
 from flask import Blueprint, request, Flask, json, current_app
+from dbModel import WebPage, db
 
 bp = Blueprint("api", __name__)
 
@@ -9,7 +10,8 @@ def get_webpage_info():
     """
     Gets information about webpage from ipstack
     """
-    return '200'
+
+    return '1'
 
 
 @bp.route("/api", methods=["POST"])
@@ -22,8 +24,24 @@ def add_webpage_info():
     if url is None:
         return '500'
 
+    # get data from ipstack
     ipstack = Ipstack.Ipstack(current_app.config["SECRET_KEY"])
     results = ipstack.fetch_data_about_url(url)
+
+    # get only relevant fields
+    results = {x: results[x] for x in current_app.config['IPSTACK_FIELDS']}
+
+    # check if url is DB
+    # the bottleneck here is that first we have to ask API
+    # about webpage URL so this request always consumes available requests amount
+    is_url_in_db = WebPage.query.with_entities(
+        WebPage.web_id
+    ).filter(WebPage.web_ip == results['ip']).\
+        first()
+
+    # if there is no webpage data in DB add it
+    if not is_url_in_db:
+        add_new_url(results)
 
     return json.dumps(results)
 
@@ -33,4 +51,19 @@ def delete_webpage_info():
     """
     Deleted cached data about webpage from the database
     """
-    return '201'
+    json_data = request.json
+    url = json_data['url']
+
+    return '200'
+
+
+def add_new_url(results: dict):
+    """
+    Adds new url into database
+    """
+    wp = WebPage(
+        **{'web_{}'.format(field): results[field]
+         for field in results.keys()}
+    )
+    db.session.add(wp)
+    db.session.commit()
