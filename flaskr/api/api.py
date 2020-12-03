9e1de85c8547268da4ffe96bd1d2caff9ee1f96f
Adrian Kuboszek <adrian.kuboszek@nokia.com>
2020-12-03 23:51:10 +0100

    Added GET request implementation

--- flaskr/api/api.py
+++ flaskr/api/api.py
@@ -1,7 +1,7 @@
 import socket
 
 from .models import Ipstack
-from flask import Blueprint, request, Flask, json, current_app
+from flask import Blueprint, request, Flask, json, current_app, jsonify
 from dbModel import WebPage, db
 
 
@@ -15,10 +15,24 @@ def get_webpage_info():
     """
     json_data = request.json
     url = json_data['url']
+
     if url is None:
         return '500'
 
-    return '1'
+    ip = url_to_ip(url)
+    ip_id = get_ip_id(ip)
+
+    if ip_id == 0:
+        return '404'
+
+    # Get data for found ip id
+    result = WebPage.query.filter(WebPage.web_id == ip_id).first()
+
+    # convert result into dict
+    result = {field: result.__dict__["web_" + field]
+              for field in current_app.config['IPSTACK_FIELDS']}
+
+    return json.dumps(result, ensure_ascii=False, default=str)
 
 
 @ bp.route("/api", methods=["POST"])
@@ -31,19 +45,15 @@ def add_webpage_info():
     if url is None:
         return '500'
 
-    # if ip adress is passed do nothing
-    try:
-        url = socket.inet_aton(url)
-    except socket.error:
-        # else convert to ip address
-        url = socket.gethostbyname(url)
+    # unify provided address
+    ip = url_to_ip(url)
 
-    if is_url_in_db(url):
+    if get_ip_id(ip) > 0:
         return '208'  # already reported
 
     # get data from ipstack
     ipstack = Ipstack.Ipstack(current_app.config["SECRET_KEY"])
-    results = ipstack.fetch_data_about_url(url)
+    results = ipstack.fetch_data_about_url(ip)
 
     # get only relevant fields
     results = {x: results[x] for x in current_app.config['IPSTACK_FIELDS']}
@@ -63,29 +73,52 @@ def delete_webpage_info():
     return '200'
 
 
-def add_new_url(results: dict):
+def add_new_url(data: dict):
     """
     Adds new url into database
+
+    @param data: dict of url fields to be added into DB 
+
+    @returns: None
     """
     wp = WebPage(
-        **{'web_{}'.format(field): results[field]
-           for field in results.keys()}
+        **{'web_{}'.format(field): data[field]
+           for field in data.keys()}
     )
     db.session.add(wp)
     db.session.commit()
 
 
-def is_url_in_db(ip: str) -> bool:
+def get_ip_id(ip: str) -> int:
     """
-    Checks if provided ip is in db
+    Gets ip id in DB
 
-    @param ip - ip adress to be searched for
+    @param ip: ip adress to be searched for
 
-    @returns True / False 
+    @returns: int of ip id > 0 if exists else 0
     """
-    is_url_in_db = WebPage.query.with_entities(
+    ip_id = WebPage.query.with_entities(
         WebPage.web_id
     ).filter(WebPage.web_ip == ip).\
         first()
 
-    return False if is_url_in_db is None else True
+    return 0 if ip_id is None else ip_id
+
+
+def url_to_ip(url: str) -> str:
+    """
+    Converts url to ip. If IP provided returns it
+
+    @param url: url address to be converted into ip
+
+    @returns ip adress of url 
+    """
+
+    # if ip adress is passed do nothing
+    try:
+        url = socket.inet_aton(url)
+    except socket.error:
+        # else convert to ip address
+        url = socket.gethostbyname(url)
+
+    return url
